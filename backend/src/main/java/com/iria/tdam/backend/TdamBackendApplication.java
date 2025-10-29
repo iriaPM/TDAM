ff538d5398ea3174dbb393ed446a5d5e58e6ea96
iriaPM <c22305863@mytudublin.ie>
2025-10-29 16:13:47 +0000

    backend - springboot set up

---
+++ backend/src/main/java/com/iria/tdam/backend/TdamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iria.tdam.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdamBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/iria/tdam/backend/TdamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iria.tdam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
